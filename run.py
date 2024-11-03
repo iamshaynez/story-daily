3bdf44a5aca9bb560ce3d3fa7bfbd273b6d0a511
Xiaowen.Z <xiaowen.z@outlook.com>
2024-11-03 19:53:02 +0800

    update

--- run.py
+++ run.py
@@ -244,8 +244,8 @@ def batch_process_multi_thread(from_date=datetime.now(), to_date=datetime.now(),
 
 
 if __name__ == "__main__":
-    str_from = "2023-01-01"
-    str_to = "2023-02-15"
+    str_from = "2021-01-01"
+    str_to = "2021-12-31"
     from_date=datetime.strptime(str_from, '%Y-%m-%d')
     to_date=datetime.strptime(str_to, '%Y-%m-%d')
 
